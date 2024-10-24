2e8a0251d858ab2aba08737b33a3d3a627e8f374
demetriotorgan <demetriotorgan@hotmail.com>
2024-10-24 19:19:15 -0300

    commit02

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 
 //Conectando com o banco
 mongoose
